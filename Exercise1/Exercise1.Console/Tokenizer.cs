a960598da28ccc555af1c0487be4711d788952c7
Ronan Moriarty <ronan.moriarty@hotmail.co.uk>
2026-03-03 19:36:45 +0000

    Add Tokenizer

---
+++ Exercise1/Exercise1.Console/Tokenizer.cs
@@ -0,0 +1,37 @@
+﻿using System.Text;
+
+namespace Exercise1.Console;
+
+public class Tokenizer
+{
+    public async IAsyncEnumerable<string> Parse(Stream input)
+    {
+        using var streamReader = new StreamReader(input);
+        string line;
+        while ((line = await streamReader.ReadLineAsync()) is not null)
+        {
+            var stringBuilder = new StringBuilder();
+            var previousCharacterIsLetter = false;
+            foreach (var c in line)
+            {
+                if (char.IsLetter(c)) // we can add test cases with multi-digit numbers to force updating this to char.IsLetterOrDigit(), but not required for now
+                {
+                    stringBuilder.Append(c);
+                    previousCharacterIsLetter = true;
+                }
+                else
+                {
+                    if (previousCharacterIsLetter)
+                    {
+                        // Reached the end of a word
+                        yield return stringBuilder.ToString();
+                        stringBuilder.Clear();
+                    }
+
+                    yield return c.ToString();
+                    previousCharacterIsLetter = false;
+                }
+            }
+        }
+    }
+}
---
+++ Exercise1/Exercise1.Tests/TokenizerTests.cs
@@ -0,0 +1,20 @@
+﻿using System.Text;
+using Exercise1.Console;
+using NUnit.Framework;
+
+namespace Exercise1.Tests;
+
+[TestFixture]
+public class TokenizerTests
+{
+    [TestCase("Hello, World!", new[] { "Hello", ",", " ", "World", "!" })]
+    public async Task CanParseStreamIntoTokens(string input, string[] expected)
+    {
+        using var memoryStream = new MemoryStream(Encoding.UTF8.GetBytes(input));
+
+        var asyncEnumerable = new Tokenizer().Parse(memoryStream);
+
+        var result = await asyncEnumerable.ToListAsync(CancellationToken.None);
+        Assert.That(result, Is.EquivalentTo(expected));
+    }
+}
