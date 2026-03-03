8072f763601cdc88ddae606a28feb7340239f355
Ronan Moriarty <ronan.moriarty@hotmail.co.uk>
2026-03-03 18:19:35 +0000

    Add ContainsLetterFilter

---
+++ Exercise1/Exercise1.Console/Filters/ContainsLetterFilter.cs
@@ -0,0 +1,9 @@
+﻿namespace Exercise1.Console.Filters;
+
+public class ContainsLetterFilter(char deniedLetter)
+{
+    public bool Filter(string input)
+    {
+        return input is null || !input.Contains(deniedLetter);
+    }
+}
---
+++ Exercise1/Exercise1.Tests/ContainsLetterFilterTests.cs
@@ -0,0 +1,19 @@
+﻿using Exercise1.Console.Filters;
+using NUnit.Framework;
+
+namespace Exercise1.Tests;
+
+[TestFixture]
+public class ContainsLetterFilterTests
+{
+    [TestCase("hat", 'a', false)]
+    [TestCase("hat", 'b', true)]
+    [TestCase(null, 'b', true)]
+    public void OnlyReturnsTrueWhenDeniedCharacterNotFound(string input,
+        char deniedCharacter,
+        bool expected)
+    {
+        var result = new ContainsLetterFilter(deniedCharacter).Filter(input);
+        Assert.That(result, Is.EqualTo(expected));
+    }
+}
