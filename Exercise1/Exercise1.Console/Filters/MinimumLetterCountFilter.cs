605238818aea52bf7375641b53f0d2c676707d10
Ronan Moriarty <ronan.moriarty@hotmail.co.uk>
2026-03-03 18:11:23 +0000

    Add MinimumNumberOfLettersFilter

---
+++ Exercise1/Exercise1.Console/Filters/MinimumLetterCountFilter.cs
@@ -0,0 +1,9 @@
+﻿namespace Exercise1.Console.Filters;
+
+public class MinimumLetterCountFilter(int minimumNumberOfLetters)
+{
+    public bool Filter(string input)
+    {
+        return input.Length >= 3;
+    }
+}
---
+++ Exercise1/Exercise1.Tests/MinimumLetterCountFilterTests.cs
@@ -0,0 +1,18 @@
+﻿using Exercise1.Console.Filters;
+using NUnit.Framework;
+
+namespace Exercise1.Tests;
+
+[TestFixture]
+public class MinimumLetterCountFilterTests
+{
+    [TestCase("hat", 3, true)]
+    [TestCase("ha", 3, false)]
+    public void OnlyReturnsTrueWhenMinimumNumberOfLettersFound(string input,
+        int minimumNumberOfLetters,
+        bool expected)
+    {
+        var result = new MinimumLetterCountFilter(minimumNumberOfLetters).Filter(input);
+        Assert.That(result, Is.EqualTo(expected));
+    }
+}
